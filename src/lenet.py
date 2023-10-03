cd84e562bf1f473fe0e9ccd4a9db6813e43896e2
Sebastian Alonso <sebasagpf@gmail.com>
2023-10-03 01:54:16 -0400

    First implementation of lenet5 using MNIST v1.0

--- src/lenet.py
+++ src/lenet.py
@@ -1,36 +1,64 @@
 import torch as t
 import numpy as np
 import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torchinfo import summary
+
+
+batch_size = 32
+num_classes = 10
+learning_rate = 0.001
+num_epochs = 10
+device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+
+
+#Import dataset and load it
+
+train = datasets.MNIST(root='./data',
+                           train=True,
+                           download=True,
+                           transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))]))
+
+test = datasets.MNIST(root='./data',
+                            train=False,
+                            download=True,
+                            transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))]))
+
+dataset = DataLoader(train, batch_size=batch_size, shuffle=True)
+
+testdataset = DataLoader(test, batch_size=batch_size, shuffle=True)
+
+
+
 
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-        # MaxPool2d(kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
         # Conv2d(1, 6, 5)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 6, 5),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(1, 6, 5, 1, 0), #conv layer 1 (input layer)
+            nn.ReLU(), #activation function
+            nn.AvgPool2d(2, 2) #pooling layer 1
         )
         # Conv2d(6, 16, 5)
         self.conv2 = nn.Sequential(
-            nn.Conv2d(6, 16, 5),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(6, 16, 5,1,0),  #conv layer 2
+            nn.ReLU(), #activation function
+            nn.AvgPool2d(2, 2) #pooling layer 2
         )
         # Linear(16*5*5, 120)
         self.fc1 = nn.Sequential(
-            nn.Linear(16*5*5, 120),
-            nn.ReLU()
+            nn.Linear(16*5*5, 120), #fully connected layer 1
+            nn.ReLU() #activation function
         )
         # Linear(120, 84)
         self.fc2 = nn.Sequential(
-            nn.Linear(120, 84),
-            nn.ReLU()
+            nn.Linear(120, 84), #fully connected layer 2
+            nn.ReLU() #activation function
         )
         # Linear(84, 10)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, num_classes) #fully connected layer 3 (output layer)
 
     def forward(self, x):
         # Conv2d(1, 6, 5)
@@ -45,3 +73,52 @@ class LeNet(nn.Module):
         # Linear(84, 10)
         x = self.fc3(x)
         return x
+
+
+model = LeNet().to(device)
+
+cost = nn.CrossEntropyLoss() #cost function
+optimizer = t.optim.Adam(model.parameters(), lr=learning_rate) #optimizer
+
+def train():
+    total_step = len(dataset)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(dataset):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            #forward pass
+            outputs = model(images)
+            loss = cost(outputs, labels)
+
+            #backwards pass and optimizer step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+def test():
+    with t.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in testdataset:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = t.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+
+
+
+train()
+test()
+
+
+
+#summary(model=model, input_size=(1, 1, 28, 28), col_width=20,col_names=['input_size', 'output_size', 'num_params', 'trainable'], row_settings=['var_names'], verbose=0)
