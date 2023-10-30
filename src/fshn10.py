d8b8358cb5dfb22e627b3902ce1c06cd459b1a14
Sebastian Alonso <sebasagpf@gmail.com>
2023-10-29 20:54:31 -0400

    v1

--- src/fshn10.py
+++ src/fshn10.py
@@ -1,96 +1,54 @@
 import torch as t
-import numpy as np
 import torch.nn as nn
+from lenet import LeNet as LeNetOriginal
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from torchinfo import summary
-
-#TODO
-# 1. Test other activation functions to see if they work different (RELU V SIGMOID)
-# 2. Test other optimizers to see if they work different (ADAM V SGD)
-# 3. Test other loss functions to see if they work different (CROSSENTROPY V SQUARED ERROR)
-# 4. Test AvgPooling vs MaxPooling
-# 5. Test different batch sizes
-
 
 batch_size = 32
 num_classes = 10
 learning_rate = 0.001
-num_epochs = 13
+num_epochs = 15
 device = t.device('cuda' if t.cuda.is_available() else 'cpu')
 
 # Import dataset and load it
-
-train = datasets.MNIST(root='./data',
+train = datasets.FashionMNIST(root='./data',
                        train=True,
                        download=True,
                        transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(),
                                                      transforms.Normalize((0.1307,), (0.3081,))]))
 
-test = datasets.MNIST(root='./data',
+test = datasets.FashionMNIST(root='./data',
                       train=False,
                       download=True,
                       transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(),
                                                     transforms.Normalize((0.1307,), (0.3081,))]))
 
-dataset = DataLoader(train, batch_size=batch_size, shuffle=True)
 
+dataset = DataLoader(train, batch_size=batch_size, shuffle=True)
 testdataset = DataLoader(test, batch_size=batch_size, shuffle=True)
 
 
-class LeNet(nn.Module):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        # Conv2d(1, 6, 5)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 6, 5, 1, 0),  # conv layer 1 (input layer)
-            nn.ReLU(),  # activation function
-            nn.AvgPool2d(2, 2)  # pooling layer 1
-        )
-        # Conv2d(6, 16, 5)
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(6, 16, 5, 1, 0),  # conv layer 2
-            nn.ReLU(),  # activation function
-            nn.AvgPool2d(2, 2)  # pooling layer 2
-        )
-        # Linear(16*5*5, 120)
-        self.fc1 = nn.Sequential(
-            nn.Linear(16 * 5 * 5, 120),  # fully connected layer 1
-            nn.ReLU()  # activation function
-        )
-        # Linear(120, 84)
-        self.fc2 = nn.Sequential(
-            nn.Linear(120, 84),  # fully connected layer 2
-            nn.ReLU()  # activation function
-        )
-        # Linear(84, 10)
-        self.fc3 = nn.Linear(84, num_classes)  # fully connected layer 3 (output layer)
 
-    def forward(self, x):
-        # Conv2d(1, 6, 5)
-        x = self.conv1(x)
-        # Conv2d(6, 16, 5)
-        x = self.conv2(x)
-        # Linear(16*5*5, 120)
-        x = x.view(x.size()[0], -1)  # reshape tensor into (batch_size, n=16*5*5) i think
-        x = self.fc1(x)
-        # Linear(120, 84)
-        x = self.fc2(x)
-        # Linear(84, 10)
-        x = self.fc3(x)
-        return x
+model = LeNetOriginal()
+#model.load_state_dict(t.load('best_model.pth'))
+model.load_state_dict(t.load('frozenfc2_model.pth'))
+model = model.to(device)
 
 
-model = LeNet().to(device)
+#Freeze Layers
+for name,param in model.named_parameters():
+    if 'fc2' in name:
+        param.requires_grad = False
+
+for name, param in model.named_parameters():
+    if 'fc2' in name:
+        param.requires_grad = False
 
 cost = nn.CrossEntropyLoss()  # cost function
 optimizer = t.optim.Adam(model.parameters(), lr=learning_rate)  # optimizer
 
-
-
-
-## by batch or each individual step?
 def train():
+    print("training...")
     total_step = len(dataset)
     global_lowest_loss = float('inf')
     best_epoch = 1
@@ -117,11 +75,11 @@ def train():
         if avg_loss < global_lowest_loss:
             global_lowest_loss = avg_loss
             best_epoch = epoch + 1
-            t.save(model.state_dict(), 'best_model.pth')
+            t.save(model.state_dict(), 'frozenfc2_model.pth')
+            t.save(model.state_dict(), 'fshn_best_model.pth')
             print("New Lowest Loss in Network: {:.4f} at Epoch [{}/{}]".format(global_lowest_loss, best_epoch, num_epochs))
 
-    print("Lowest Loss in Network {:.4f} at Epoch [{}/{}]".format(global_lowest_loss, best_epoch, num_epochs))
-
+    print("Lowest Loss in Network {: .4f} at Epoch [{}/{}]".format(global_lowest_loss, best_epoch, num_epochs))
 
 def test():
     with t.no_grad():  # no gradient descent
@@ -137,8 +95,7 @@ def test():
 
         print('Accuracy of the network on the test images: {} %'.format(100 * correct / total))
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
     train()
     test()
-
-# summary(model=model, input_size=(1, 1, 28, 28), col_width=20,col_names=['input_size', 'output_size', 'num_params', 'trainable'], row_settings=['var_names'], verbose=0)
